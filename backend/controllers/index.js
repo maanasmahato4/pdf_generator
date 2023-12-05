4606a7e890c21ed75a35db728267ed77b7810d56
maanasmahato4 <maanasmahato456@gmail.com>
2023-12-06 00:09:44 +0545

    task: logger implementation in controllers

--- backend/controllers/index.js
+++ backend/controllers/index.js
@@ -1,9 +1,22 @@
-function getAllFiles() {
+const pool = require("../database/database");
+const logger = require("../utils/logger");
 
+function getAllFiles(request, response) {
+    logger.info(`${request.url} GET /api/`, null);
+    try {
+        return response.sendStatus(200);
+    } catch (error) {
+        logger.error(`${request.url} GET /api/`, error);
+    };
 };
 
-function generateNewPdf(){
-
+function generateNewPdf(request, response) {
+    logger.info(`${request.url} POST /api/`, null);
+    try {
+        return response.sendStatus(200);
+    } catch (error) {
+        logger.error(`${request.url} POST /api/`, error);
+    };
 };
 
 module.exports = {
