3dfb993d7d49bf744cb3ba441eab64ced0793675
maanasmahato4 <maanasmahato456@gmail.com>
2023-12-06 03:31:21 +0545

    fixed: database issues

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 // Enabling environment variables
-require("dotenv");
+require("dotenv").config();
 
 // Third party libraries
 const express = require("express");
@@ -8,9 +8,35 @@ const cors = require("cors");
 // Custom
 const corsOptions = require("./utils/cors.options");
 const errorHandler = require("./utils/errorHandler");
+const pool = require("./database/database");
 
 // Initializing app
 const app = express();
+
+async function testDatabaseConnection() {
+    try {
+      // Get a client from the pool
+      const client = await pool.connect();
+  
+      // Use the client to query the database
+      const result = await client.query('SELECT NOW() as current_time');
+  
+      // Release the client back to the pool
+      client.release();
+  
+      // Log the result or perform any necessary actions
+      console.log('Current time from the database:', result.rows[0].current_time);
+      
+      return true; // Indicates successful connection and query execution
+    } catch (error) {
+      // Log and handle any errors that occur during the process
+      console.error('Error occurred while testing database connection:', error);
+      return false; // Indicates failure in connecting or executing query
+    }
+  }
+  
+  // Call the function to test the database connection
+  testDatabaseConnection();
 
 // Middlewares
 app.use(cors(corsOptions));
