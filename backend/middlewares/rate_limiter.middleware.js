7c6ac95838a9a2cec4bb0ddad16a64c2c8f97183
maanasmahato4 <maanasmahato456@gmail.com>
2023-12-05 23:21:15 +0545

    fixed: env issue

---
+++ backend/middlewares/rate_limiter.middleware.js
@@ -0,0 +1,10 @@
+const { rateLimit } = require("express-rate-limit");
+
+function limiter(interval, limit) {
+    return rateLimit({
+        windowMs: interval != null ? interval : 60 * 1000,
+        limit: limit != null ? limit : 10,
+        standardHeaders: true,
+        legacyHeaders: false
+    });
+};
