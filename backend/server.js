e6d5e8d9af5b6ca03d263562826e369247d6c55e
maanasmahato4 <maanasmahato456@gmail.com>
2023-12-06 19:17:57 +0545

    feature: helmet security

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ require("dotenv").config();
 // Third party libraries
 const express = require("express");
 const cors = require("cors");
+const helmet = require("helmet");
 
 // Custom
 const corsOptions = require("./utils/cors.options");
@@ -18,6 +19,8 @@ testDatabaseConnection();
 
 // Middlewares
 app.use(cors(corsOptions));
+app.use(helmet());
+app.use(express.json());
 app.use("/api", require("./routes"));
 
 
