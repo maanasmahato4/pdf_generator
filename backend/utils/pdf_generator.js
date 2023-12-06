1dfe8493a6af1fc9f443b6f6dd84e3f19229b5f0
maanasmahato4 <maanasmahato456@gmail.com>
2023-12-06 19:24:54 +0545

    feature: pdf generator

--- backend/utils/pdf_generator.js
+++ backend/utils/pdf_generator.js
@@ -1,10 +1,30 @@
 const pdf_document = require("pdfkit");
-
+const fs = require("fs");
+const path = require("path");
+const { generateUniqueName } = require("./utilityFunctions");
 
 // new document
 async function generatePDF(text_information) {
-    console.log(text_information);
-    const doc = new pdf_document;
+    const { title, body, conclusion } = text_information;
+    try {
+        let pdfFilePath, uniqueName;
+        uniqueName = generateUniqueName();
+        pdfFilePath = path.join(__dirname, "../uploads", `${uniqueName}.pdf`);
+        const doc = new pdf_document;
+        if (!fs.existsSync(path.join(__dirname, "../uploads"))) {
+            fs.mkdirSync(path.join(__dirname, "../uploads"));
+        };
+
+        doc.pipe(fs.createWriteStream(pdfFilePath));
+        doc.text(`Title: ${title}`);
+        doc.text(`Body: ${body}`);
+        doc.text(`Conclusion: ${conclusion}`);
+        doc.end();
+
+        return pdfFilePath;
+    } catch (error) {
+        throw new Error(error);
+    };
 };
 
 
